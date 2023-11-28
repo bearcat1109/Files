c267ddb3ead095a4ae2fa1384e4d9c210bf6559a
Berresg <92836148+bearcat1109@users.noreply.github.com>
2023-11-28 15:57:41 -0600

    Create sectionImportFunctional.sql

---
+++ Workwork/Sql/sectionImportFunctional.sql
@@ -0,0 +1,89 @@
+WITH w_scbcrse AS (
+    SELECT DISTINCT
+        scbcrse_subj_code                                       AS w_scbcrse_subj_code,
+        scbcrse_crse_numb                                       AS w_scbcrse_crse_numb,
+        MAX(scbcrse_eff_term)
+        OVER(PARTITION BY scbcrse_subj_code, scbcrse_crse_numb) AS w_scbcrse_eff_term
+    FROM
+        scbcrse
+)
+SELECT DISTINCT
+    ssbsect_crn
+    || '.'
+    || ssbsect_term_code                      AS "Section_ID",
+    scbcrse_title
+    ||
+    CASE
+            WHEN ssbsect_camp_code = '01' THEN
+                ' - TA - '
+            WHEN ssbsect_camp_code = '02' THEN
+                ' - MK - '
+            WHEN ssbsect_camp_code = '03' THEN
+                ' - BA - '
+            WHEN ssbsect_camp_code = '04' THEN
+                ' - PO - '
+            WHEN ssbsect_camp_code = '05' THEN
+                ' - MI - '
+            WHEN ssbsect_camp_code = '06' THEN
+                ' - PC - '
+            WHEN ssbsect_camp_code = '07' THEN
+                ' - WI - '
+            WHEN ssbsect_camp_code = '08' THEN
+                ' - CO - '
+            WHEN ssbsect_camp_code = '09' THEN
+                ' - TU - '
+            ELSE
+                ssbsect_camp_code || ' - '
+    END
+    ||
+    CASE
+            WHEN ssbsect_schd_code IN ( '04', '06', '08', '10', '11',
+                                        '28' ) THEN
+                'INDV'
+            WHEN ssbsect_schd_code IN ( '22', '23', '23A', '31', '22A',
+                                        '30', '27A', '26A', '27', '26',
+                                        '33', '32', '33A', '32A', '31A',
+                                        '30A' ) THEN
+                'BLND'
+            WHEN ssbsect_schd_code IN ( '25', '24', '25A', '24A', '21',
+                                        '20', '21A', '20A', '35', '34',
+                                        '14', '15', '05' ) THEN
+                'ONLN'
+            WHEN ssbsect_schd_code IN ( '16', '17', '29',  '18', '19'  ) THEN
+                'INTN'
+            WHEN ssbsect_schd_code IN ( '03', '12', '13', '00A', '01A' ) THEN
+                'TRAD'
+            WHEN ssbsect_schd_code IN ( '02', '07' ) THEN
+                'EXTN'
+            --WHEN ssbsect_schd_code IN ( '05' ) THEN
+                --'CORR - '
+            WHEN ssbsect_schd_code IN ( '00', '01' ) THEN
+                'REGC'
+            WHEN ssbsect_schd_code IN ( '09' ) THEN
+                'ZERO'
+            WHEN ssbsect_schd_code LIKE 'CE%' THEN
+                'CNED'
+            ELSE
+                'TRAD'
+    END                                       AS "Section_Name",
+    scbcrse_subj_code
+    || ' '
+    || scbcrse_crse_numb                      AS "Course_Code",
+    gobtpac_external_user || '@nsuok.edu'     AS "Instructor",
+    to_char(stvterm_start_date, 'MM/DD/YYYY') AS "Start_Date",
+    to_char(stvterm_end_date, 'MM/DD/YYYY')   AS "End_Date",
+    stvterm_code                              AS "Term"
+FROM
+         ssbsect
+    JOIN w_scbcrse ON w_scbcrse_subj_code = ssbsect_subj_code
+                      AND w_scbcrse_crse_numb = ssbsect_crse_numb
+    JOIN scbcrse ON scbcrse_subj_code = w_scbcrse_subj_code
+                    AND scbcrse_crse_numb = w_scbcrse_crse_numb
+                    AND scbcrse_eff_term = w_scbcrse_eff_term
+    JOIN stvterm ON ssbsect_term_code = stvterm_code
+    JOIN ssrmeet ON ssrmeet_crn = ssbsect_crn
+                    AND ssrmeet_term_code = ssbsect_term_code
+    LEFT JOIN gobtpac ON gobtpac_pidm = ssksels.f_section_instructor(ssbsect_crn, ssbsect_term_code, 'Y', '')
+WHERE
+    ssbsect_ssts_code = 'A'
+    AND stvterm_code = :main_lb_terms.stvterm_code
