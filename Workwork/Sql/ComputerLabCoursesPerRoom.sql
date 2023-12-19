dd41e3d5ea367b3ff2c5efb7c7b6dd4382efcc0d
Berresg <92836148+bearcat1109@users.noreply.github.com>
2023-12-19 14:24:15 -0600

    Update ComputerLabCoursesPerRoom.sql

--- Workwork/Sql/ComputerLabCoursesPerRoom.sql
+++ Workwork/Sql/ComputerLabCoursesPerRoom.sql
@@ -19,12 +19,18 @@ FROM
                                       'BT B8', 'SC 270', 'SC LL46', 'BAGLEY 327', 'SS 106',
                                       'FITCTR 205', 'WILSON G18', 'WILSON 415', 'BABT 129', 'BAED 113',
                                       'BALA 118', 'BALA 130', 'BALB 106', 'BALB 110', 'BALA 230',
-                                      'BALA 234', 'NSM 209', 'SYNAR 205' )
+                                      'BALA 234', 'NSM 209', 'SYNAR 205',
+                                      -- Added 12-19-2023
+                                      'BAED 129', 'BAED 209', 'BASC 249', 'FA 201', 'LIB 118A', 'WILSON 238'
+                                      )
     )
 WHERE
-    term_code =
-    (SELECT DISTINCT MAX(stvterm_code)
-            FROM stvterm
-            WHERE stvterm_start_date < sysdate
-            AND stvterm_desc NOT LIKE 'CE%'
-    );
+    term_code IN ('202420', '202410', '202330')
+--    (SELECT DISTINCT MAX(stvterm_code)
+--            FROM stvterm
+--            WHERE stvterm_start_date < sysdate
+--            AND stvterm_desc NOT LIKE 'CE%'
+--    );
+ORDER BY
+    term_code ASC
+;
