72cf13bdbffb5d9bab9e27c4d6318001bb4c6165
Berresg <92836148+bearcat1109@users.noreply.github.com>
2024-01-23 16:18:08 -0600

    Update TermImport.sql

--- Workwork/Sql/TermImport.sql
+++ Workwork/Sql/TermImport.sql
@@ -6,3 +6,29 @@ select distinct
     to_char(stvterm_end_date, 'DD/MM/YYYY') End_Date
 from stvterm
 where stvterm_code > '202320' AND stvterm_code != '999999';
+
+
+
+-- Written 8-28-2023 by Gabriel Berres, SLL Term Import
+-- Updated 1/23/2023 Programmers + Rob
+select distinct
+    stvterm_code Term_Code,
+    CASE
+        WHEN substr(stvterm_code, 5, 2) = 30 THEN
+            substr(stvterm_code, 0, 4)
+        ELSE
+            to_char(substr(stvterm_code, 0, 4) - 1)
+    END
+    || ' '
+    ||     CASE
+        WHEN substr(stvterm_code, 5, 2) = 10 THEN
+            'Summer'
+        WHEN substr(stvterm_code, 5, 2) = 20 THEN
+            'Fall'
+        WHEN substr(stvterm_code, 5, 2) = 30 THEN
+            'Spring'
+    END Term_Name,
+    to_char((stvterm_start_date - 28), 'DD/MM/YYYY' ) Start_Date,
+    to_char(stvterm_end_date, 'DD/MM/YYYY') End_Date
+from stvterm
+where stvterm_code > '202320' AND stvterm_code != '999999'
