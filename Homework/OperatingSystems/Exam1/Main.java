6a037d83a6f2a0989f3c37d7055f436f5ca382bf
Berresg <92836148+bearcat1109@users.noreply.github.com>
2024-02-14 15:24:59 -0600

    Create Main.java

---
+++ Homework/OperatingSystems/Exam1/Main.java
@@ -0,0 +1,17 @@
+// CS Operating Systems exam 01, 2/15/2024 Gabriel Berres
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.package.class
+
+public class Main {
+    public static void main(String[] args)
+    {
+        //System.out.println("Ohayou, sekai");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to exam 1, please enter a buffer size:");
+        int BUFFER_SIZE = scan.nextInt();
+        System.out.println("Your buffer size was: " + BUFFER_SIZE);
+
+    }
+}
