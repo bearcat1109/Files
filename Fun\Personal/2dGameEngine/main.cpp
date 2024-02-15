230b624a970b37e56cbb6f3afb1ab2d9ec820d77
Berresg <92836148+bearcat1109@users.noreply.github.com>
2024-02-15 07:33:13 -0600

    Update main.cpp

--- Fun\Personal/2dGameEngine/main.cpp
+++ Fun\Personal/2dGameEngine/main.cpp
@@ -10,17 +10,40 @@ Game *game = nullptr;
 
 int main(int argc, char* argv[])
 {
+
+	const int FPS = 60;
+	// How much time between frames
+	const int frameDelay = 1000 / FPS;
+
+	//
+	Uint32 frameStart;
+	int frameTime;
+
 	// Game object
 	game = new Game();
 
-	game->init("SpruceEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	// Picked 800x640 because x600 doesn't divide by 32
+	game->init("ArchangelEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, false);
 
 	// Do game ops while game is running. Self-explanatory
 	while (game->running())
 	{
+		// Get frame
+		frameStart = SDL_GetTicks();
+
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		// How long the frame takes
+		frameTime = SDL_GetTicks() - frameStart;
+
+		// Delay frame if needed
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 
 	game->clean();
