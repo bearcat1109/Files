889ab057b24030027d0690f0a97f720fff7cb12f
Berresg <92836148+bearcat1109@users.noreply.github.com>
2024-02-15 07:32:49 -0600

    Create map.cpp

---
+++ Fun\Personal/2dGameEngine/map.cpp
@@ -0,0 +1,85 @@
+#include "map.h"
+#include "textureManager.h"
+
+int lvl1[20][25] =
+{
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+};
+
+Map::Map()
+{
+	dirt = textureManager::LoadTexture("assets/dirt.png");
+	grass = textureManager::LoadTexture("assets/grass.png");
+	water = textureManager::LoadTexture("assets/water.png");
+
+	loadMap(lvl1);
+	
+	// Give starting locations
+	src.x = src.y = 0;
+	src.w = dest.w = 32;
+	src.h = dest.h = 32;
+
+	dest.x = dest.y = 0;
+}
+
+void Map::loadMap(int arr[20][25])
+{
+	for (int row = 0; row < 20; row++)
+	{
+		for (int column = 0; column < 25; column++)
+		{
+			map[row][column] = arr[row][column];
+		}
+	}
+}
+
+void Map::drawMap()
+{
+	int type = 0;
+	for (int row = 0; row < 20; row++)
+	{
+		for (int column = 0; column < 25; column++)
+		{
+			// Set type to be the row column pair of the tile
+			type = map[row][column];
+
+			// Multiply by 32 since that is the tile size
+			dest.x = column * 32;
+			dest.y = row * 32;
+
+			switch (type)
+			{
+			case 0:
+				textureManager::Draw(water, src, dest);
+				break;
+			case 1:
+				textureManager::Draw(grass, src, dest);
+				break;
+			case 2:
+				textureManager::Draw(dirt, src, dest);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+}
