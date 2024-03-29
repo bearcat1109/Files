539b48099fc59b125c9ad01a8cdb6685716059f4
Berresg <92836148+bearcat1109@users.noreply.github.com>
2024-03-29 16:19:40 -0500

    Update sectionImportFunctional.sql

--- Workwork/Sql/sectionImportFunctional.sql
+++ Workwork/Sql/sectionImportFunctional.sql
@@ -1,3 +1,4 @@
+-- Re-added Instructors 1-24-2023 Gabriel + Rob
 WITH w_scbcrse AS (
     SELECT DISTINCT
         scbcrse_subj_code                                       AS w_scbcrse_subj_code,
@@ -12,41 +13,47 @@ SELECT DISTINCT
     || '.'
     || ssbsect_term_code                      AS "Section_ID",
     scbcrse_title
+    || '-' ||
+    ssbsect_crn
+    || '.'
+    || ssbsect_term_code
     ||
     CASE
             WHEN ssbsect_camp_code = '01' THEN
-                ' - TA - '
+                '.TA.'
             WHEN ssbsect_camp_code = '02' THEN
-                ' - MK - '
+                '.MK.'
             WHEN ssbsect_camp_code = '03' THEN
-                ' - BA - '
+                '.BA.'
             WHEN ssbsect_camp_code = '04' THEN
-                ' - PO - '
+                '.PO.'
             WHEN ssbsect_camp_code = '05' THEN
-                ' - MI - '
+                '.MI.'
             WHEN ssbsect_camp_code = '06' THEN
-                ' - PC - '
+                '.PC.'
             WHEN ssbsect_camp_code = '07' THEN
-                ' - WI - '
+                '.WI.'
             WHEN ssbsect_camp_code = '08' THEN
-                ' - CO - '
+                '.CO.'
             WHEN ssbsect_camp_code = '09' THEN
-                ' - TU - '
+                '.TU.'
             ELSE
-                ssbsect_camp_code || ' - '
+                ssbsect_camp_code || '.'
     END
     ||
     CASE
             WHEN ssbsect_schd_code IN ( '04', '06', '08', '10', '11',
                                         '28' ) THEN
                 'INDV'
-            WHEN ssbsect_schd_code IN ( '22', '23', '23A', '31', '22A',
+            -- moved 22A/23A to HFLX 3/29/24 berresg
+            WHEN ssbsect_schd_code IN ( '22', '23', '31',
                                         '30', '27A', '26A', '27', '26',
                                         '33', '32', '33A', '32A', '31A',
                                         '30A' ) THEN
                 'BLND'
+            -- moved 34/35 to VCM
             WHEN ssbsect_schd_code IN ( '25', '24', '25A', '24A', '21',
-                                        '20', '21A', '20A', '35', '34',
+                                        '20', '21A', '20A', --'35', '34',
                                         '14', '15', '05' ) THEN
                 'ONLN'
             WHEN ssbsect_schd_code IN ( '16', '17', '29',  '18', '19'  ) THEN
@@ -63,14 +70,23 @@ SELECT DISTINCT
                 'ZERO'
             WHEN ssbsect_schd_code LIKE 'CE%' THEN
                 'CNED'
+            -- Changes 3/29/24 berresg
+            WHEN 
+                ssbsect_schd_code IN ('22A', '23A')
+            THEN
+                'HFLX'
+            WHEN
+                ssbsect_schd_code IN ('34', '35')
+            THEN
+                'VCM'
             ELSE
                 'TRAD'
     END                                       AS "Section_Name",
     scbcrse_subj_code
     || ' '
     || scbcrse_crse_numb                      AS "Course_Code",
-    gobtpac_external_user || '@nsuok.edu'     AS "Instructor",
-    to_char(stvterm_start_date, 'MM/DD/YYYY') AS "Start_Date",
+    nvl(gobtpac_external_user, 'placeholder') || '@nsuok.edu'     AS "Instructor",
+    to_char((stvterm_start_date - 28), 'MM/DD/YYYY') AS "Start_Date",
     to_char(stvterm_end_date, 'MM/DD/YYYY')   AS "End_Date",
     stvterm_code                              AS "Term"
 FROM
@@ -83,7 +99,9 @@ FROM
     JOIN stvterm ON ssbsect_term_code = stvterm_code
     JOIN ssrmeet ON ssrmeet_crn = ssbsect_crn
                     AND ssrmeet_term_code = ssbsect_term_code
+    JOIN nsudev.wm_sll_crse ON wm_sll_crse_crse_numb = ssbsect_crse_numb
+         AND wm_sll_crse_subj_code = ssbsect_subj_code
     LEFT JOIN gobtpac ON gobtpac_pidm = ssksels.f_section_instructor(ssbsect_crn, ssbsect_term_code, 'Y', '')
 WHERE
     ssbsect_ssts_code = 'A'
-    AND stvterm_code = :main_lb_terms.stvterm_code
+    AND stvterm_code = '202430';
