09b01b78150fcee5a6a14e95ab22812b52a6b242
Berresg <92836148+bearcat1109@users.noreply.github.com>
2024-04-02 16:51:51 -0500

    Create psychMajorsWIP.sql

---
+++ Workwork/Sql/psychMajorsWIP.sql
@@ -0,0 +1,30 @@
+WITH w_sgbstdn AS 
+(
+    SELECT DISTINCT
+        sgbstdn_pidm                    w_sgbstdn_pidm,
+        MAX(sgbstdn_term_code_eff)
+        OVER(PARTITION BY sgbstdn_pidm) w_sgbstdn_term_code_eff
+    FROM
+        sgbstdn
+    WHERE 
+        sgbstdn_term_code_eff <= to_number('202430')
+)
+SELECT 
+    w_sgbstdn_pidm,
+    spriden_last_name || ', ' || spriden_first_name name_,
+    ma_stvmajr.stvmajr_desc major,
+    co_stvmajr.stvmajr_desc concentration,
+    mi_stvmajr.stvmajr_desc minor
+FROM
+    sfbetrm
+    JOIN w_sgbstdn ON w_sgbstdn_pidm = sfbetrm_pidm
+    JOIN sgbstdn ON sgbstdn_pidm = w_sgbstdn_pidm
+        AND sgbstdn_term_code_eff = w_sgbstdn_term_code_eff
+        AND sgbstdn_stst_code NOT IN ( 'IS', 'IG' )
+    JOIN stvmajr ma_stvmajr ON ma_stvmajr.stvmajr_code = sgbstdn_majr_code_1
+    LEFT JOIN stvmajr co_stvmajr ON co_stvmajr.stvmajr_code = sgbstdn_majr_code_conc_1
+    LEFT JOIN stvmajr mi_stvmajr ON mi_stvmajr.stvmajr_code = sgbstdn_majr_code_minr_1
+    JOIN spriden ON spriden_pidm = sfbetrm_pidm
+WHERE
+    sgbstdn_majr_code_1 IN ( 'P076', '6756', '3565' )
+    
