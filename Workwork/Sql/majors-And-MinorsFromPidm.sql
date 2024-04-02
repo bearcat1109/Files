27c1de4014a56b5d1399026643bbbf0bb6751ece
Berresg <92836148+bearcat1109@users.noreply.github.com>
2024-04-02 16:29:36 -0500

    Create majors-And-MinorsFromPidm.sql

---
+++ Workwork/Sql/majors-And-MinorsFromPidm.sql
@@ -0,0 +1,13 @@
+SELECT
+    sgbstdn_pidm pidm,
+    sgbstdn_term_code_eff term_eff,
+    sgbstdn_majr_code_1 major_code,
+    sgbstdn_majr_code_minr_1 minor_code,
+    mj_stvmajr.stvmajr_desc major,
+    mn_stvmajr.stvmajr_desc minor
+FROM
+    sgbstdn
+    LEFT JOIN stvmajr mj_stvmajr ON mj_stvmajr.stvmajr_code = sgbstdn_majr_code_1
+    LEFT JOIN stvmajr mn_stvmajr ON mn_stvmajr.stvmajr_code = sgbstdn_majr_code_minr_1
+WHERE
+    sgbstdn_pidm = 246610;
