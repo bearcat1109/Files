1a2ef5299537c5d25be52272e8c723d36d28f32f
Berresg <92836148+bearcat1109@users.noreply.github.com>
2024-04-11 07:44:59 -0500

    Update projectZooDatabase.sql

--- Homework/DatabaseManagement/projectZooDatabase.sql
+++ Homework/DatabaseManagement/projectZooDatabase.sql
@@ -4,10 +4,12 @@ CREATE TABLE ZOOIDEN
 (
 	animal_id INT PRIMARY KEY, 
 	animal_name VARCHAR(255),
-	caretaker_id VARCHAR(255) UNIQUE,
+	caretaker_id VARCHAR(255),
 	area_code INT,
 	feed_schedule_code INT
 );
+
+
 
 CREATE TABLE ZOOAREA
 (
@@ -17,12 +19,12 @@ CREATE TABLE ZOOAREA
 
 CREATE TABLE ZOOCARE
 (
-	caretaker_id PRIMARY KEY,
+	caretaker_id INT PRIMARY KEY,
 	caretaker_first_name VARCHAR(255),
 	caretaker_last_name VARCHAR(255),
-	caretaker_username VARCHAR(7) UNIQUE,
+	caretaker_username VARCHAR(8) UNIQUE,
 	caretaker_posn INT,
-	caretaker_phone VARCHAR(255) UNIQUE
+	caretaker_salary MONEY
 );
 
 CREATE TABLE ZOOPOSN
@@ -35,7 +37,7 @@ CREATE TABLE ZOOPOSN
 
 CREATE TABLE ZOOFEED
 (
-	feed_schedule INT UNIQUE,
+	feed_schedule_code INT UNIQUE,
 	feed_schedule_desc VARCHAR(255) 
 );
 
@@ -65,21 +67,21 @@ INSERT INTO ZOOIDEN (animal_id, animal_name, caretaker_id, area_code, feed_sched
 (22, 'Yoink the Leopard Gecko', 9, 5, 5),      -- Area code Reptile Kingdom
 (23, 'Drake the Komodo Dragon', 9, 5, 5),
 (24, 'Skyler the Blue-Tongued Skink', 10, 5, 5),
-(25, 'Frank the Python', 10, 5, 5)
+(25, 'Frank the Python', 10, 5, 5);
 
 INSERT INTO ZOOAREA (area_code, area_desc) VALUES 
 (1, 'African Safari'),
 (2, 'Jungle'),
 (3, 'Tundra'),
 (4, 'Aquarium'),
-(5, 'Reptile Kingdom')
+(5, 'Reptile Kingdom');
 
 INSERT INTO ZOOFEED (feed_schedule_code, feed_schedule_desc) VALUES 
 (1, 'Twice Daily - Morning/Evening'),
 (2, 'Once Daily - Morning'),
 (3, 'Once Daily - Evening'),
 (4, 'Once Every Two Days - Morning'),
-(5, 'Once Every Two Days - Evening')
+(5, 'Once Every Two Days - Evening');
 
 INSERT INTO ZOOCARE (caretaker_id, caretaker_first_name, caretaker_last_name, caretaker_username, 
 caretaker_posn, caretaker_salary) VALUES
@@ -87,17 +89,17 @@ caretaker_posn, caretaker_salary) VALUES
 (2, 'Sarah', 'Berres', 'berressa', 1, 45000.00),
 (3, 'Tonya', 'Garrett', 'garretto', 2, 55000.00),
 (4, 'Aaron', 'Garrett', 'garretaa', 2, 55000.00),
-(5, 'Jason', 'Tidwell', 'tidwellja', 2, 55000.00),
-(5, 'Eden', 'Wallace', 'wallac33', 2, 55000.00),
-(6, 'Lelo', 'Bekele', 'bekelele', 3, 65000.00),
-(7, 'Travis', 'Cook', 'cooktrav', 3, 65000.00),
-(8, 'Kaden', 'Scroggins', 'scroggik', 3, 65000.00),
-(9, 'Joshua', 'Simmons', 'simmonsj', 4, 80000.00),
-(10, 'Steven', 'Hensley', 'hensleys', 5, 100000.00)
+(5, 'Jason', 'Tidwell', 'tidwellj', 2, 55000.00),
+(6, 'Eden', 'Wallace', 'wallac33', 2, 55000.00),
+(7, 'Lelo', 'Bekele', 'bekelele', 3, 65000.00),
+(8, 'Travis', 'Cook', 'cooktrav', 3, 65000.00),
+(9, 'Kaden', 'Scroggins', 'scroggik', 3, 65000.00),
+(10, 'Joshua', 'Simmons', 'simmonsj', 4, 80000.00),
+(11, 'Steven', 'Hensley', 'hensleys', 5, 100000.00);
 
 INSERT INTO ZOOPOSN (caretaker_posn, caretaker_posn_desc, caretaker_weekly_hrs, caretaker_posn_salary) VALUES
 (1, 'Caretaker I', 30, 45000.00),
 (2, 'Caretaker II', 35, 55000.00),
 (3, 'Caretaker III', 40, 65000.00),
 (4, 'Caretaker & Assistant Director', 40, 80000.00),
-(5, 'Caretaker & Director', 40, 100000.00)
+(5, 'Caretaker & Director', 40, 100000.00);
